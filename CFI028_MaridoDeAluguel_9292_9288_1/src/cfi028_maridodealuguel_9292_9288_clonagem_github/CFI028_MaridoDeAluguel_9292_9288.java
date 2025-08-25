6ba7e2b247bf79caff084f4e5e046a05457e0896
Thiago-Felipi <tfelipi386@gmail.com>
2025-08-24 21:07:42 -0300

    Revert "segunbdo commit teste mudança de nome e adiçao de mensagem"
    This reverts commit 9f0e9f7e9772acfdb757dc874bb45e641fa2cf16.

--- CFI028_MaridoDeAluguel_9292_9288_1/src/cfi028_maridodealuguel_9292_9288_clonagem_github/CFI028_MaridoDeAluguel_9292_9288.java
+++ CFI028_MaridoDeAluguel_9292_9288_1/src/cfi028_maridodealuguel_9292_9288_clonagem_github/CFI028_MaridoDeAluguel_9292_9288.java
@@ -1,3 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+20:46
+
+muda muda muda 
+20:48
+mudanças feitas
+ */
+package cfi028_maridodealuguel_9292_9288_clonagem_github;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Alunos
+ */
+public class CFI028_MaridoDeAluguel_9292_9288 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TelaInicial.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
 
 
 /*
