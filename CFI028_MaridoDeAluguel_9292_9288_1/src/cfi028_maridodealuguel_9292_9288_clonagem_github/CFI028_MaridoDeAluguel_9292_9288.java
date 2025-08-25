b9ca0005bc0e5047855df37eea9ea689aef30955
Thiago-Felipi <tfelipi386@gmail.com>
2025-08-24 21:39:16 -0300

    segunbdo commit teste mudança de nome e adiçao de mensagem

--- CFI028_MaridoDeAluguel_9292_9288_1/src/cfi028_maridodealuguel_9292_9288_clonagem_github/CFI028_MaridoDeAluguel_9292_9288.java
+++ CFI028_MaridoDeAluguel_9292_9288_1/src/cfi028_maridodealuguel_9292_9288_clonagem_github/CFI028_MaridoDeAluguel_9292_9288.java
@@ -1,4 +1,5 @@
 /*
+*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,6 +13,7 @@ mudanças feitas
 package cfi028_maridodealuguel_9292_9288_clonagem_github;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
